0bcc8e48f9fa304834a1476c84dd2b231bf9b3a4
Hugo Lapa <Hugolapa888@gmail.com>
2022-05-09 23:00:57 +0100

    STart of Firebase setup

--- pages/index.js
+++ pages/index.js
@@ -90,6 +90,30 @@ export default function Home() {
         
       </main>
 
+      <script type="module">
+      // Import the functions you need from the SDKs you need
+      import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+      import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-analytics.js";
+      // TODO: Add SDKs for Firebase products that you want to use
+      // https://firebase.google.com/docs/web/setup#available-libraries
+
+      // Your web app's Firebase configuration
+      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+      const firebaseConfig = {
+        apiKey: "AIzaSyBM7i4uyhaH8Pb7bxQD3EIQ7DddcRz34sM",
+        authDomain: "todoapp-b9a56.firebaseapp.com",
+        projectId: "todoapp-b9a56",
+        storageBucket: "todoapp-b9a56.appspot.com",
+        messagingSenderId: "734592971207",
+        appId: "1:734592971207:web:d5419a2f981a4e14c269e2",
+        measurementId: "G-1Z4ES19CSK"
+      };
+
+      // Initialize Firebase
+      const app = initializeApp(firebaseConfig);
+      const analytics = getAnalytics(app);
+      </script> 
+
 
      
     </div>
